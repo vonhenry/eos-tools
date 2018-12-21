347f5d1a06b99ec23b17e81c7892efba53caa0c6
vonhenry <amorofroma@gmail.com>
2018-12-21 21:55:45 +0800

    update

--- cpptest/main.cpp
+++ cpptest/main.cpp
@@ -3,49 +3,61 @@
 #include <chrono>
 #include <iostream>
 
+
+#include <eosio/chain/types.hpp>
+
+#include <eosio/chain/controller.hpp>
+#include <eosio/chain/exceptions.hpp>
+#include <eosio/chain/block.hpp>
+#include <eosio/chain/contract_types.hpp>
+
+
+#include <fc/network/message_buffer.hpp>
+#include <fc/network/ip.hpp>
+#include <fc/io/json.hpp>
+#include <fc/io/raw.hpp>
+#include <fc/log/appender.hpp>
+#include <fc/container/flat.hpp>
+#include <fc/reflect/variant.hpp>
+#include <fc/crypto/rand.hpp>
+#include <fc/exception/exception.hpp>
+
+#include <boost/asio/ip/tcp.hpp>
+#include <boost/asio/ip/host_name.hpp>
+#include <boost/asio/steady_timer.hpp>
+#include <boost/intrusive/set.hpp>
+
+using mvo = fc::mutable_variant_object;
+
 using namespace boost::asio;
+using namespace eosio::chain;
+
+struct lwc_init_message {
+   lwc_init_message():header(),active_schedule(),blockroot_merkle(){}
+   signed_block_header     header;
+   producer_schedule_type  active_schedule;
+   incremental_merkle      blockroot_merkle;
+};
+
+
+template<typename T>
+T dejsonify(const string& s) {
+   return fc::json::from_string(s).as<T>();
+}
 
 int main()
 {
-   io_service ioservice;
 
-   steady_timer timer1{ioservice, std::chrono::seconds{3}};
-   timer1.wait();
-   std::cout << "--1--" << std::endl;
+   auto var = mvo()  ("header",           block_header())
+                     ("active_schedule",  producer_schedule_type())
+                     ("blockroot_merkle", incremental_merkle());
 
-   steady_timer timer2{ioservice, std::chrono::seconds{3}};
-   timer2.wait();
-   std::cout << "--2--" << std::endl;
+   auto str =  fc::json::to_string(var);
+
+   std::cout << str << std::endl;
+
+   return 0;
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
