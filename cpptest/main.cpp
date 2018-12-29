90c04a30a267c14a4afa8d85f9016101ebf1a02f
vonhenry <amorofroma@gmail.com>
2018-12-29 18:44:09 +0800

    update

--- cpptest/main.cpp
+++ cpptest/main.cpp
@@ -34,127 +34,93 @@ using mvo = fc::mutable_variant_object;
 using namespace boost::asio;
 using namespace eosio::chain;
 
-namespace eosio {  namespace chain { namespace resource_limits {
 
 
-   template<typename T>
-   struct ratio {
-      static_assert(std::is_integral<T>::value, "ratios must have integral types");
-      T numerator;
-      T denominator;
-   };
 
-   template<typename T>
-   ratio<T> make_ratio(T n, T d) {
-      return ratio<T>{n, d};
+template<typename T>
+struct ratio {
+   static_assert(std::is_integral<T>::value, "ratios must have integral types");
+   T numerator;
+   T denominator;
+};
+
+template<typename T>
+ratio<T> make_ratio(T n, T d) {
+   return ratio<T>{n, d};
+}
+
+template<typename T>
+T operator* (T value, const ratio<T>& r) {
+//   eosio_assert(r.numerator == T(0) || std::numeric_limits<T>::max() / r.numerator >= value, "Usage exceeds maximum value representable after extending for precision");
+   return (value * r.numerator) / r.denominator;
+}
+
+const static uint64_t Precision = 100;
+
+struct accumulator
+{
+   accumulator( uint32_t value_ws, uint32_t times_ws ):
+   last_ordinal(0),value_ws(value_ws),times_ws(times_ws),value_ex(0),times_ex(0){}
+
+   uint32_t   last_ordinal;    ///< The ordinal of the last period which has contributed to the average
+   uint64_t   value_ex;       
+   uint64_t   times_ex;     
+
+   void addvalue( uint64_t value, uint32_t ordinal ) {
+      add( value_ex, value, ordinal, value_ws, false );
+      add( times_ex, 1, ordinal, times_ws );
    }
 
-   template<typename T>
-   T operator* (T value, const ratio<T>& r) {
-      EOS_ASSERT(r.numerator == T(0) || std::numeric_limits<T>::max() / r.numerator >= value, rate_limiting_state_inconsistent, "Usage exceeds maximum value representable after extending for precision");
-      return (value * r.numerator) / r.denominator;
-   }
+private:
+   uint32_t   value_ws;
+   uint32_t   times_ws;
 
-   template<typename UnsignedIntType>
-   constexpr UnsignedIntType integer_divide_ceil(UnsignedIntType num, UnsignedIntType den ) {
-      return (num / den) + ((num % den) > 0 ? 1 : 0);
-   }
-
-
-   template<typename LesserIntType, typename GreaterIntType>
-   constexpr bool is_valid_downgrade_cast =
-         std::is_integral<LesserIntType>::value &&  // remove overloads where type is not integral
-         std::is_integral<GreaterIntType>::value && // remove overloads where type is not integral
-         (std::numeric_limits<LesserIntType>::max() <= std::numeric_limits<GreaterIntType>::max()); // remove overloads which are upgrades not downgrades
-
-/**
- * Specialization for Signedness matching integer types
- */
-   template<typename LesserIntType, typename GreaterIntType>
-   constexpr auto downgrade_cast(GreaterIntType val) ->
-   std::enable_if_t<is_valid_downgrade_cast<LesserIntType,GreaterIntType> && std::is_signed<LesserIntType>::value == std::is_signed<GreaterIntType>::value, LesserIntType>
-   {
-      const GreaterIntType max = std::numeric_limits<LesserIntType>::max();
-      const GreaterIntType min = std::numeric_limits<LesserIntType>::min();
-      EOS_ASSERT( val >= min && val <= max, rate_limiting_state_inconsistent, "Casting a higher bit integer value ${v} to a lower bit integer value which cannot contain the value, valid range is [${min}, ${max}]", ("v", val)("min", min)("max",max) );
-      return LesserIntType(val);
-   };
-
-/**
- * Specialization for Signedness mismatching integer types
- */
-   template<typename LesserIntType, typename GreaterIntType>
-   constexpr auto downgrade_cast(GreaterIntType val) ->
-   std::enable_if_t<is_valid_downgrade_cast<LesserIntType,GreaterIntType> && std::is_signed<LesserIntType>::value != std::is_signed<GreaterIntType>::value, LesserIntType>
-   {
-      const GreaterIntType max = std::numeric_limits<LesserIntType>::max();
-      const GreaterIntType min = 0;
-      EOS_ASSERT( val >= min && val <= max, rate_limiting_state_inconsistent, "Casting a higher bit integer value ${v} to a lower bit integer value which cannot contain the value, valid range is [${min}, ${max}]", ("v", val)("min", min)("max",max) );
-      return LesserIntType(val);
-   };
-
-/**
- *  This class accumulates and exponential moving average based on inputs
- *  This accumulator assumes there are no drops in input data
- *
- *  The value stored is Precision times the sum of the inputs.
- */
-
-   const static uint64_t Precision = 1000*1000;
-   static constexpr uint64_t max_raw_value = std::numeric_limits<uint64_t>::max() / Precision;
-
-   struct accumulator
-   {
-      accumulator():last_ordinal(0),value_ex(0){}
-      uint32_t   last_ordinal;  ///< The ordinal of the last period which has contributed to the average
-      uint64_t   value_ex;      ///< The current average pre-multiplied by Precision
-
-      uint64_t average() const {
-         return integer_divide_ceil(value_ex, Precision);
-      }
-
-      void add( uint64_t units, uint32_t ordinal, uint32_t window_size ) {
-//      eosio_assert( units <= max_raw_value, "Usage exceeds maximum value representable after extending for precision");
-
-         auto value_ex_contrib = downgrade_cast<uint64_t>(integer_divide_ceil((uint128_t)units * Precision, (uint128_t)window_size));
-//      EOS_ASSERT(std::numeric_limits<decltype(value_ex)>::max() - value_ex >= value_ex_contrib, rate_limiting_state_inconsistent, "Overflow in accumulated value when adding usage!");
-
-         if( last_ordinal != ordinal ) {
-//         EOS_ASSERT( ordinal > last_ordinal, resource_limit_exception, "new ordinal cannot be less than the previous ordinal" );
-            if( (uint64_t)last_ordinal + window_size > (uint64_t)ordinal ) {
-               const auto delta = ordinal - last_ordinal; // clearly 0 < delta < window_size
-               const auto decay = make_ratio(
-                     (uint64_t)window_size - delta,
-                     (uint64_t)window_size
-               );
-
-               value_ex = value_ex * decay;
-            } else {
-               value_ex = 0;
-            }
-
+   void add( uint64_t& src, uint64_t value, uint32_t ordinal, uint32_t window_size, bool update_ordinal = true ) {
+      if( last_ordinal != ordinal ) {
+//         eosio_assert( ordinal > last_ordinal, "new ordinal cannot be less than the previous ordinal" );
+         if( (uint64_t)last_ordinal + window_size > (uint64_t)ordinal ) {
+            const auto delta = ordinal - last_ordinal; // clearly 0 < delta < window_size
+            const auto decay = make_ratio(
+               (uint64_t)window_size - delta,
+               (uint64_t)window_size
+            );
+            src = src * decay;
+         } else {
+            src = 0;
+         }
+         if( update_ordinal ){
             last_ordinal = ordinal;
          }
-
-         value_ex += value_ex_contrib;
       }
-   };
-
-
-}}}
+      src += value * Precision;
+   }
+};
 
 
 int main()
 {
-   eosio::chain::resource_limits::exponential_moving_average_accumulator acc;
+   int day = 3600*24;
+   int minute = 60;
+   accumulator acc( day, minute );
 
+   for( auto i = 0; i < 24*60*4; ++i){
+      acc.addvalue( 50, 60 * (i + 1) );
+      if ( i % 60 == 0 ){
 
-   acc.add( 50, 100, 200);
-   acc.add( 50, 200, 200);
+         auto res = 300000 * (i/60) - acc.value_ex;
+         std::cout << acc.value_ex  << "  " <<  res << "      " << res * 100.0 / (300000 * (i/60)) << "      "<< acc.times_ex << std::endl;
+      }
+   }
 
+   accumulator bcc( day, minute );
 
-   acc.add( 50, 500, 200);
-   acc.add( 50, 550, 200);
+   for( auto i = 0; i < 24*4; ++i){
+      bcc.addvalue( 50, 3600 * (i + 1) );
+      auto res = 5000 * (i + 1) - bcc.value_ex;
+      std::cout << bcc.value_ex  << "  " <<  res << "      " << res * 100.0 / (5000 * (i+1)) << "      "<< bcc.times_ex << std::endl;
+
+   }
+
 
 
    return 0;
@@ -162,6 +128,8 @@ int main()
 
 }
 
+
+// 24*60*50 = 72000
 
 
 
