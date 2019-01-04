3a93b9e7b71b930d6671b1a03ebec42d211cf8c0
vonhenry <amorofroma@gmail.com>
2019-01-04 18:33:01 +0800

    update

--- cpptest/main.cpp
+++ cpptest/main.cpp
@@ -34,94 +34,72 @@ using mvo = fc::mutable_variant_object;
 using namespace boost::asio;
 using namespace eosio::chain;
 
+using std::vector;
+
+using boost::asio::ip::tcp;
+using boost::asio::ip::address_v4;
+using boost::asio::ip::host_name;
+using boost::intrusive::rbtree;
+using boost::multi_index_container;
+
+using fc::time_point;
+using fc::time_point_sec;
+using eosio::chain::transaction_id_type;
+using eosio::chain::name;
+using mvo = fc::mutable_variant_object;
+namespace bip = boost::interprocess;
 
 
-
-template<typename T>
-struct ratio {
-   static_assert(std::is_integral<T>::value, "ratios must have integral types");
-   T numerator;
-   T denominator;
+struct lwc_section_info {
+   lwc_section_info(int a, int b):first(a),last(b){}
+   int                   first;
+   int                   last;
 };
 
-template<typename T>
-ratio<T> make_ratio(T n, T d) {
-   return ratio<T>{n, d};
-}
+typedef multi_index_container<
+   lwc_section_info,
+   indexed_by<
+      ordered_unique<
+         tag< by_id >,
+         member < lwc_section_info,
+            int,
+            &lwc_section_info::first > >
+   >
+>
+   ibc_section_index;
 
-template<typename T>
-T operator* (T value, const ratio<T>& r) {
-//   eosio_assert(r.numerator == T(0) || std::numeric_limits<T>::max() / r.numerator >= value, "Usage exceeds maximum value representable after extending for precision");
-   return (value * r.numerator) / r.denominator;
-}
-
-const static uint64_t Precision = 100;
-
-struct accumulator
-{
-   accumulator( uint32_t value_ws, uint32_t times_ws ):
-   last_ordinal(0),value_ws(value_ws),times_ws(times_ws),value_ex(0),times_ex(0){}
-
-   uint32_t   last_ordinal;    ///< The ordinal of the last period which has contributed to the average
-   uint64_t   value_ex;       
-   uint64_t   times_ex;     
-
-   void addvalue( uint64_t value, uint32_t ordinal ) {
-      add( value_ex, value, ordinal, value_ws, false );
-      add( times_ex, 1, ordinal, times_ws );
-   }
-
-private:
-   uint32_t   value_ws;
-   uint32_t   times_ws;
-
-   void add( uint64_t& src, uint64_t value, uint32_t ordinal, uint32_t window_size, bool update_ordinal = true ) {
-      if( last_ordinal != ordinal ) {
-//         eosio_assert( ordinal > last_ordinal, "new ordinal cannot be less than the previous ordinal" );
-         if( (uint64_t)last_ordinal + window_size > (uint64_t)ordinal ) {
-            const auto delta = ordinal - last_ordinal; // clearly 0 < delta < window_size
-            const auto decay = make_ratio(
-               (uint64_t)window_size - delta,
-               (uint64_t)window_size
-            );
-            src = src * decay;
-         } else {
-            src = 0;
-         }
-         if( update_ordinal ){
-            last_ordinal = ordinal;
-         }
-      }
-      src += value * Precision;
-   }
-};
-
+ibc_section_index             local_sections;
 
 int main()
 {
-   int day = 3600*24;
-   int minute = 60;
-   accumulator acc( day, minute );
+   lwc_section_info a(1,1), b(2,2), c(3,3), d(4,4),e(4,5);
 
-   for( auto i = 0; i < 24*60*4; ++i){
-      acc.addvalue( 50, 60 * (i + 1) );
-      if ( i % 60 == 0 ){
+   auto [aa,bb ] = local_sections.insert(e);
 
-         auto res = 300000 * (i/60) - acc.value_ex;
-         std::cout << acc.value_ex  << "  " <<  res << "      " << res * 100.0 / (300000 * (i/60)) << "      "<< acc.times_ex << std::endl;
-      }
+   local_sections.erase(aa);
+
+   auto [cc,dd ] = local_sections.insert(d);
+
+   local_sections.insert(a);
+   local_sections.insert(b);
+   local_sections.insert(c);
+
+   local_sections.insert(d);
+   local_sections.insert(d);
+   local_sections.insert(c);
+   local_sections.insert(c);
+   local_sections.insert(a);
+
+
+
+//   local_sections.insert(local_sections.end(),a);
+//   local_sections.insert(local_sections.end(),b);
+//   local_sections.insert(local_sections.end(),c);
+//   local_sections.insert(local_sections.end(),d);
+
+   for( auto it = local_sections.rbegin(); it != local_sections.rend(); ++it ){
+      ilog("======${n1},${n2}",("n1",it->first)("n2",it->last));
    }
-
-   accumulator bcc( day, minute );
-
-   for( auto i = 0; i < 24*4; ++i){
-      bcc.addvalue( 50, 3600 * (i + 1) );
-      auto res = 5000 * (i + 1) - bcc.value_ex;
-      std::cout << bcc.value_ex  << "  " <<  res << "      " << res * 100.0 / (5000 * (i+1)) << "      "<< bcc.times_ex << std::endl;
-
-   }
-
-
 
    return 0;
 
@@ -129,7 +107,7 @@ int main()
 }
 
 
-// 24*60*50 = 72000
+
 
 
 
